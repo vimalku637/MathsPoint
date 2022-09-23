dec65bd5a025ef58442bec0b3a816161ee3f209c
Vimal Kumar <vimalku637@gmail.com>
2022-09-24 00:05:40 +0530

    some chnages

--- app/src/main/java/com/vk/mathspoint/MainActivity.kt
+++ app/src/main/java/com/vk/mathspoint/MainActivity.kt
@@ -1,35 +1,73 @@
 package com.vk.mathspoint
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.google.android.gms.ads.rewarded.RewardItem
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
+import com.vk.mathspoint.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    private var myWebView : WebView? = null
+    private var TAG = "MainActivity>>"
 
-    /*banner ad-mob*/
-    lateinit var mAdView : AdView
+    private lateinit var binding: ActivityMainBinding
+    private var mInterstitialAd: InterstitialAd?=null
+    private var mRewardedAd: RewardedAd?=null
+    private var adRequest: AdRequest?=null
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        /*banner ad-mob*/
-        MobileAds.initialize(this) {}
 
-        mAdView = findViewById(R.id.adView)
-        val adRequest = AdRequest.Builder().build()
-        mAdView.loadAd(adRequest)
+        binding=DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+        binding.viewmodel=this
 
-        mAdView.adListener = object: AdListener() {
+        /** ads initialization */
+        adsInitialization()
+
+        if(isInternetAvailable(this@MainActivity)) {
+            binding.webView?.webViewClient = MyWebViewClient()
+            binding.webView?.loadUrl("https://mathspoint.info")
+            binding.webView?.settings?.javaScriptEnabled = true
+            binding.webView?.settings?.loadsImagesAutomatically = true
+            binding.webView?.addJavascriptInterface(WebAppInterface(this), "Android")
+        }else{
+            AlertDialog.Builder(this@MainActivity)
+                .setTitle("No internet connection found!!")
+                .setMessage("Please make sure you have internet connection before use the application.")
+                .setPositiveButton(
+                    "Ok"
+                ) { _, _ -> finishAffinity() }
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show()
+        }
+    }
+
+    private fun adsInitialization() {
+        /**ad-mob ads*/
+        MobileAds.initialize(this@MainActivity)
+
+        adRequest = AdRequest.Builder().build()
+        binding.adView.loadAd(adRequest)
+
+        binding.adView.adListener = object: AdListener() {
             override fun onAdLoaded() {
                 // Code to be executed when an ad finishes loading.
             }
@@ -53,23 +91,97 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        myWebView=findViewById(R.id.webView)
+        /**interstitial ads*/
+        initializationInterstitialAds(adRequest!!)
+        /**rewarded ads*/
+        initializationRewardedAds(adRequest!!)
 
-        if(isInternetAvailable(this)) {
-            myWebView?.webViewClient = MyWebViewClient()
-            myWebView?.loadUrl("https://mathspoint.info")
-            myWebView?.settings?.javaScriptEnabled = true
-            myWebView?.settings?.loadsImagesAutomatically = true
-            myWebView?.addJavascriptInterface(WebAppInterface(this), "Android")
-        }else{
-            AlertDialog.Builder(this)
-                .setTitle("No internet connection found!!")
-                .setMessage("Please make sure you have internet connection before use the application.")
-                .setPositiveButton(
-                    "Ok"
-                ) { _, _ -> finishAffinity() }
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .show()
+    }
+
+    private fun initializationInterstitialAds(adRequest: AdRequest) {
+        InterstitialAd.load(this,"ca-app-pub-1719443042342371/4872331653", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError.toString())
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
+
+        mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+            override fun onAdClicked() {
+                // Called when a click is recorded for an ad.
+                Log.d(TAG, "Ad was clicked.")
+            }
+
+            override fun onAdDismissedFullScreenContent() {
+                // Called when ad is dismissed.
+                Log.d(TAG, "Ad dismissed fullscreen content.")
+                mInterstitialAd = null
+            }
+
+            override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                // Called when ad fails to show.
+                Log.e(TAG, "Ad failed to show fullscreen content.")
+                mInterstitialAd = null
+            }
+
+            override fun onAdImpression() {
+                // Called when an impression is recorded for an ad.
+                Log.d(TAG, "Ad recorded an impression.")
+            }
+
+            override fun onAdShowedFullScreenContent() {
+                // Called when ad is shown.
+                Log.d(TAG, "Ad showed fullscreen content.")
+            }
+        }
+    }
+
+    private fun initializationRewardedAds(adRequest: AdRequest) {
+        RewardedAd.load(this,"ca-app-pub-1719443042342371/4844411497", adRequest, object : RewardedAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError?.toString())
+                mRewardedAd = null
+            }
+
+            override fun onAdLoaded(rewardedAd: RewardedAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mRewardedAd = rewardedAd
+            }
+        })
+
+        mRewardedAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+            override fun onAdClicked() {
+                // Called when a click is recorded for an ad.
+                Log.d(TAG, "Ad was clicked.")
+            }
+
+            override fun onAdDismissedFullScreenContent() {
+                // Called when ad is dismissed.
+                // Set the ad reference to null so you don't show the ad a second time.
+                Log.d(TAG, "Ad dismissed fullscreen content.")
+                mRewardedAd = null
+            }
+
+            override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                // Called when ad fails to show.
+                Log.e(TAG, "Ad failed to show fullscreen content.")
+                mRewardedAd = null
+            }
+
+            override fun onAdImpression() {
+                // Called when an impression is recorded for an ad.
+                Log.d(TAG, "Ad recorded an impression.")
+            }
+
+            override fun onAdShowedFullScreenContent() {
+                // Called when ad is shown.
+                Log.d(TAG, "Ad showed fullscreen content.")
+            }
         }
     }
 
@@ -100,8 +212,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         // Check if the key event was the Back button and if there's history
-        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView?.canGoBack()!!) {
-            myWebView?.goBack()
+        if (keyCode == KeyEvent.KEYCODE_BACK && binding.webView?.canGoBack()!!) {
+            binding.webView?.goBack()
+
+            /** show Rewarded ads*/
+            if (mRewardedAd != null) {
+                mRewardedAd?.show(this, OnUserEarnedRewardListener() {
+                    fun onUserEarnedReward(rewardItem: RewardItem) {
+                        var rewardAmount = rewardItem.amount
+                        var rewardType = rewardItem.type
+                        Log.d(TAG, "User earned the reward.")
+                    }
+                })
+            } else {
+                Log.d(TAG, "The rewarded ad wasn't ready yet.")
+            }
+
             return true
         }
         // If it wasn't the Back key or there's no web page history, bubble up to the default
@@ -109,11 +235,21 @@ class MainActivity : AppCompatActivity() {
         return super.onKeyDown(keyCode, event)
     }
 
-    fun isInternetAvailable(context: Context): Boolean {
+    private fun isInternetAvailable(context: Context): Boolean {
         val connectivityManager =
             context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkInfo = connectivityManager.activeNetworkInfo
         return networkInfo != null && networkInfo.isAvailable && networkInfo.isConnected
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        /** show interstitial ads*/
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this@MainActivity)
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.")
+        }
     }
 }
 
