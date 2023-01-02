a305421f1e084008b5da1f3d4b19919187933a2e
Vimal Kumar <vimalku637@gmail.com>
2023-01-02 23:07:52 +0530

    changes

--- app/src/main/java/com/vk/mathspoint/MainActivity.kt
+++ app/src/main/java/com/vk/mathspoint/MainActivity.kt
@@ -7,7 +7,6 @@ import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.KeyEvent
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
@@ -16,20 +15,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.android.gms.ads.*
-import com.google.android.gms.ads.interstitial.InterstitialAd
-import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
-import com.google.android.gms.ads.rewarded.RewardItem
-import com.google.android.gms.ads.rewarded.RewardedAd
-import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 import com.vk.mathspoint.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
-    private var TAG = "MainActivity>>"
-
     private lateinit var binding: ActivityMainBinding
-    private var mInterstitialAd: InterstitialAd?=null
-    private var mRewardedAd: RewardedAd?=null
     private var adRequest: AdRequest?=null
 
     @SuppressLint("ClickableViewAccessibility")
@@ -65,7 +54,7 @@ class MainActivity : AppCompatActivity() {
         MobileAds.initialize(this@MainActivity)
 
         adRequest = AdRequest.Builder().build()
-        binding.adView.loadAd(adRequest)
+        binding.adView.loadAd(adRequest!!)
 
         binding.adView.adListener = object: AdListener() {
             override fun onAdLoaded() {
@@ -88,99 +77,6 @@ class MainActivity : AppCompatActivity() {
             override fun onAdClosed() {
                 // Code to be executed when the user is about to return
                 // to the app after tapping on an ad.
-            }
-        }
-
-        /**interstitial ads*/
-        initializationInterstitialAds(adRequest!!)
-        /**rewarded ads*/
-        initializationRewardedAds(adRequest!!)
-
-    }
-
-    private fun initializationInterstitialAds(adRequest: AdRequest) {
-        InterstitialAd.load(this,"ca-app-pub-1719443042342371/4872331653", adRequest, object : InterstitialAdLoadCallback() {
-            override fun onAdFailedToLoad(adError: LoadAdError) {
-                Log.d(TAG, adError.toString())
-                mInterstitialAd = null
-            }
-
-            override fun onAdLoaded(interstitialAd: InterstitialAd) {
-                Log.d(TAG, "Ad was loaded.")
-                mInterstitialAd = interstitialAd
-            }
-        })
-
-        mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
-            override fun onAdClicked() {
-                // Called when a click is recorded for an ad.
-                Log.d(TAG, "Ad was clicked.")
-            }
-
-            override fun onAdDismissedFullScreenContent() {
-                // Called when ad is dismissed.
-                Log.d(TAG, "Ad dismissed fullscreen content.")
-                mInterstitialAd = null
-            }
-
-            override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
-                // Called when ad fails to show.
-                Log.e(TAG, "Ad failed to show fullscreen content.")
-                mInterstitialAd = null
-            }
-
-            override fun onAdImpression() {
-                // Called when an impression is recorded for an ad.
-                Log.d(TAG, "Ad recorded an impression.")
-            }
-
-            override fun onAdShowedFullScreenContent() {
-                // Called when ad is shown.
-                Log.d(TAG, "Ad showed fullscreen content.")
-            }
-        }
-    }
-
-    private fun initializationRewardedAds(adRequest: AdRequest) {
-        RewardedAd.load(this,"ca-app-pub-1719443042342371/4844411497", adRequest, object : RewardedAdLoadCallback() {
-            override fun onAdFailedToLoad(adError: LoadAdError) {
-                Log.d(TAG, adError?.toString())
-                mRewardedAd = null
-            }
-
-            override fun onAdLoaded(rewardedAd: RewardedAd) {
-                Log.d(TAG, "Ad was loaded.")
-                mRewardedAd = rewardedAd
-            }
-        })
-
-        mRewardedAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
-            override fun onAdClicked() {
-                // Called when a click is recorded for an ad.
-                Log.d(TAG, "Ad was clicked.")
-            }
-
-            override fun onAdDismissedFullScreenContent() {
-                // Called when ad is dismissed.
-                // Set the ad reference to null so you don't show the ad a second time.
-                Log.d(TAG, "Ad dismissed fullscreen content.")
-                mRewardedAd = null
-            }
-
-            override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
-                // Called when ad fails to show.
-                Log.e(TAG, "Ad failed to show fullscreen content.")
-                mRewardedAd = null
-            }
-
-            override fun onAdImpression() {
-                // Called when an impression is recorded for an ad.
-                Log.d(TAG, "Ad recorded an impression.")
-            }
-
-            override fun onAdShowedFullScreenContent() {
-                // Called when ad is shown.
-                Log.d(TAG, "Ad showed fullscreen content.")
             }
         }
     }
@@ -214,20 +110,6 @@ class MainActivity : AppCompatActivity() {
         // Check if the key event was the Back button and if there's history
         if (keyCode == KeyEvent.KEYCODE_BACK && binding.webView?.canGoBack()!!) {
             binding.webView?.goBack()
-
-            /** show Rewarded ads*/
-            if (mRewardedAd != null) {
-                mRewardedAd?.show(this, OnUserEarnedRewardListener() {
-                    fun onUserEarnedReward(rewardItem: RewardItem) {
-                        var rewardAmount = rewardItem.amount
-                        var rewardType = rewardItem.type
-                        Log.d(TAG, "User earned the reward.")
-                    }
-                })
-            } else {
-                Log.d(TAG, "The rewarded ad wasn't ready yet.")
-            }
-
             return true
         }
         // If it wasn't the Back key or there's no web page history, bubble up to the default
@@ -240,16 +122,6 @@ class MainActivity : AppCompatActivity() {
             context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkInfo = connectivityManager.activeNetworkInfo
         return networkInfo != null && networkInfo.isAvailable && networkInfo.isConnected
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        /** show interstitial ads*/
-        if (mInterstitialAd != null) {
-            mInterstitialAd?.show(this@MainActivity)
-        } else {
-            Log.d("TAG", "The interstitial ad wasn't ready yet.")
-        }
     }
 }
 
